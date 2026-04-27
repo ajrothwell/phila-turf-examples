2e702b041cfb8c70e0ab03696e2adf8bfe2db445
andy.rothwell <rothwell.andy@gmail.com>
2026-04-27 12:38:31 -0400

    page 3: zoom out one more level

--- src/examples/distance/DistancePage.vue
+++ src/examples/distance/DistancePage.vue
@@ -95,7 +95,7 @@ const popupName = computed<string>(() => {
     </template>
 
     <template #map>
-      <DemoMap :zoom="10.7" @click="onMapClick">
+      <DemoMap :zoom="9.7" @click="onMapClick">
         <MapSearchControl
           position="top-left"
           placeholder="Search a Philly address"
